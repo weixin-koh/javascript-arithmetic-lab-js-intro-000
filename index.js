2c720ed7721f1164a3a18e2ad2d5c57cd5c0856b
Wei Xin Koh <e0201350@u.nus.edu>
2020-03-27 17:01:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ function divide(num1, num2) {
   return num1 / num2
 }
 
+function increment(num) {
+  return ++num
+}
+
+function decrement(num) {
+  return --num
+}
+
 function makeInt(string) {
   return parseInt(string, 10)
 }
